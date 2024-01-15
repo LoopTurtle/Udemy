e789fbc0a4749d658aefd47595a67d7e3e5f31b8
LoopTurtle <theloopturtle@gmail.com>
2024-01-15 20:56:11 +0000

    VS projects

---
+++ Design Patterns in C#/1 - SOLID principles/1 - Single Responsibility Principle/SRP/Program.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using static System.Console;
+
+namespace DotNetDesignPatternDemos.SOLID.SRP
+{
+    // just stores a couple of journal entries and ways of
+    // working with them
+    public class Journal
+    {
+        private readonly List<string> entries = new List<string>();
+
+        private static int count = 0;
+
+        public int AddEntry(string text)
+        {
+            entries.Add($"{++count}: {text}");
+            return count; // memento pattern!
+        }
+
+        public void RemoveEntry(int index)
+        {
+            entries.RemoveAt(index);
+        }
+
+        public override string ToString()
+        {
+            return string.Join(Environment.NewLine, entries);
+        }
+
+        // breaks single responsibility principle
+        public void Save(string filename, bool overwrite = false)
+        {
+            File.WriteAllText(filename, ToString());
+        }
+
+        public void Load(string filename)
+        {
+
+        }
+
+        public void Load(Uri uri)
+        {
+
+        }
+    }
+
+    // handles the responsibility of persisting objects
+    public class Persistence
+    {
+        public void SaveToFile(Journal journal, string filename, bool overwrite = false)
+        {
+            if (overwrite || !File.Exists(filename))
+                File.WriteAllText(filename, journal.ToString());
+        }
+    }
+
+    public class Demo
+    {
+        static void Main(string[] args)
+        {
+            var j = new Journal();
+            j.AddEntry("I cried today.");
+            j.AddEntry("I ate a bug.");
+            WriteLine(j);
+
+            var p = new Persistence();
+            var filename = @"C:\Users\dloft\Documents\Code Projects\_Udemy\Udemy\Design Patterns in C#\1 - SOLID principles\1 - Single Responsibility Principle\SRP\journal.txt";
+            p.SaveToFile(j, filename);
+
+            //Process.Start(filename);
+
+            // Use Process.Start with "notepad.exe" to open the text file
+            Process.Start("notepad.exe", filename);
+        }
+    }
+}
