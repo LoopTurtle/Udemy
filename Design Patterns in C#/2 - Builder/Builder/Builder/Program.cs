6224374fbcc9ab84bd46f8bd258c3178d5743a9d
LoopTurtle <theloopturtle@gmail.com>
2024-03-04 20:17:39 +0000

    Builder

---
+++ Design Patterns in C#/2 - Builder/Builder/Builder/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Text;
+
+namespace Coding.Exercise
+{
+    // ***Target output:***
+    //public class Person
+    //{
+    //    public string Name;
+    //    public int Age;
+    //}
+
+    public class CodeBuilder
+    {
+        private StringBuilder sb = new StringBuilder();
+        private string className;
+
+        public CodeBuilder(string className)
+        {
+            this.className = className;
+            sb.AppendLine($"public class {className}");
+            sb.AppendLine("{");
+        }
+
+        public CodeBuilder AddField(string fieldName, string fieldType)
+        {
+            sb.AppendLine($"  public {fieldType} {fieldName};");
+            return this;
+        }
+
+
+        public override string ToString()
+        {
+            sb.AppendLine("}");
+            return sb.ToString();
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var cb = new CodeBuilder("Person").AddField("Name", "string").AddField("Age", "int").AddField("Northern", "bool").AddField("Hairstyle", "string");
+            Console.WriteLine(cb);
+        }
+    }
+}
+
